bd853c297a8a4f4fc5b72de9508c8bdd080601f2
DawnXi <1738465125@qq.com>
2017-11-07 18:34:30 +0800

    integration

---
+++ js/getimg.js
@@ -0,0 +1,15 @@
+
+document.getElementByClassName(".prev").click(alert(););
+
+
+
+var data={
+	src:""
+};
+$(".pic-box").each(function(){
+    data.push($(this).attr("data-src"));
+    return data;
+})
+
+
+["http://s3.mogucdn.com/mlcdn/917393/171023_30haeeic4eb190ijk5ba6f2k4dkd5_1600x2250.jpg","http://s3.mogucdn.com/mlcdn/917393/171023_6b74b90che9962b1b08alke8c2g12_640x900.jpg","http://s3.mogucdn.com/mlcdn/917393/171024_7g4flbc76201g5hf5lad96k3ia15g_1600x2250.jpg","http://s11.mogucdn.com/mlcdn/917393/171025_57667ickhbgjea5hc5695lg8840jf_813x1143.jpg","http://s3.mogucdn.com/mlcdn/917393/171024_07964d61lbk3eee71ei5k80hi0ii6_640x900.jpg","http://s3.mogucdn.com/mlcdn/917393/171024_3912ki2ah6b3107044l4j02325i9f_1600x2250.jpg","http://s3.mogucdn.com/mlcdn/917393/171024_8e217kh1lief6gd57fdlj2142j034_1600x2250.jpg","http://s3.mogucdn.com/mlcdn/917393/171025_5h246319fhak7756gka49ljhike15_640x900.jpg","http://s11.mogucdn.com/mlcdn/917393/171025_80bfj0aa17bhj1f20h59eii8246b8_640x900.jpg","http://s3.mogucdn.com/mlcdn/917393/171023_605b38ee0i6dd52ib22ckcefll9i3_1246x1752.jpg","http://s11.mogucdn.com/mlcdn/917393/171018_2b10hih231i530a8cdc7082h8ciel_1600x2250.jpg","http://s3.mogucdn.com/mlcdn/917393/171022_19hg7769ddb47c8j457f70jfc1c4b_1600x2250.jpg","http://s3.mogucdn.com/mlcdn/917393/171023_6b74b90che9962b1b08alke8c2g12_640x900.jpg","http://s3.mogucdn.com/mlcdn/917393/171021_096ajahggecca611fklbhjh4e84jj_640x900.jpg","http://s3.mogucdn.com/mlcdn/917393/171023_279kk3g0l2lclec9c8iaa984d7h8i_1600x2250.jpg","http://s3.mogucdn.com/mlcdn/917393/171020_6cde8l2g2h2417018i59f1g3a20j1_1379x1940.jpg","http://s3.mogucdn.com/mlcdn/917393/171022_2611a1gak4aec5gle0d31ll34c00h_1323x1861.jpg","http://s3.mogucdn.com/mlcdn/917393/171023_0i826dj5jhe2jd2f5ff509i20j214_640x900.jpg","http://s3.mogucdn.com/mlcdn/917393/171023_0ck53labc3k888ha0ilk063bi80j9_828x1164.jpg","http://s3.mogucdn.com/mlcdn/917393/171023_0g2kkl6kk760jk27539bgdac09cjd_1600x2250.jpg"]
